7aff1d78bd374ee88347d2f3a309be2c6a09f928
E-Ward <s182964@ucs.ac.uk>
2017-03-23 21:16:47 +0000

    Weekly portfolio

---
+++ Lists/Lists/Lists/List.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lists
+{
+    class List<T> //T is the type of data stored in a perticular instance of List
+    {
+        public class Node // declare a class called node
+        {
+            public T Data; //Each node holds a value of type T
+            public Node Next; //Each node has a reference to the next node in the list
+        }
+
+        private Node head; //Has a head variable to point to the start of the list
+        private int count = 0; // Tells us where we are in the list and how big it is
+
+        public T Head { get { return head.Data; } } // When the head is called it returns head.Data
+
+        public List()
+        {
+            head = null; //Head is set to null
+        }
+
+        public void InsertBeginning(T data)
+        {
+            Node newNode = new Node(); // Creates a new node
+            newNode.Data = data; // Places the data into the new node that was just created 
+            if(head == null) // if the head is empty
+            {
+                head = newNode; //sets the node that was just created as the head
+            }
+            else
+            {
+                newNode.Next = head; // everything bumps to the right and the head becomes the new node
+                head = newNode;
+            }
+            count++;
+        }
+
+        public T RemoveBeginning() { //Returns the value of T
+            T ret = default(T);
+            if(head != null)
+            {
+                ret = head.Data;
+                if(head.Next == null) //Checks if the head is null
+                {
+                    head = null;
+                }
+                else // Makes the new node the head
+                {
+                    head = head.Next;
+                }
+            }
+            count--;
+            return ret;
+        }
+
+        public void InsertEnd(T data) //Returns the value of T
+        {
+            if (head == null)
+            {
+                InsertBeginning(data);
+            }
+            else
+            {
+                Node current = head;
+                while(current.Next != null)
+                {
+                    current = current.Next;
+                }
+                Node newNode = new Node();
+                newNode.Data = data;
+                newNode.Next = null;
+                current.Next = newNode;
+            }
+        }
+
+        public T RemoveAfter(int count)
+        {
+            T ret = default(T);
+
+            Node current = head;
+            int counter = 0;
+
+            while (current != null)
+            {
+                if(current.Next != null)
+                {
+                    ret = current.Next.Data;
+                    current.Next = current.Next.Next;
+                    counter--; //Takes 1 from the count
+                }
+
+                break;
+            }
+
+            current = current.Next;
+
+            return ret; //Returns the value of ret
+
+        }
+    }
+}
---
+++ Lists/Lists/Lists/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Stack<T>
+    {
+        private List<T> stack;
+        private int count; //Shows the size of the stack
+
+        public int Count { get { return count; } }
+
+        public Stack()
+        {
+            stack = new List<T>();
+        }
+
+        ~Stack() //Deconstructor
+        {
+            while (!IsEmpty()) //While it is not empty remove the top until it is empty
+            {
+                T output; //Output of value T
+                Pop(out output);
+            }
+        }
+
+        public void Push(T data)
+        {
+            count++;
+            stack.InsertBeginning(data);
+        }
+
+        public bool Pop(out T output)
+        {
+            output = default(T);
+            if (IsEmpty())
+            {
+                return false;
+            }
+            output = stack.RemoveBeginning(); //Removes the top item from the stack
+            count--; //Takes one from the size of the stack
+            return true;
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        public T Peek() //Allows us to look at the top value of the stack without touching it
+        {
+            return stack.Head; //Returns the data that is contained in the head 
+        }
+
+    }
+}
